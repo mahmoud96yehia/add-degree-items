1e61d7af480dfbe14de19a78e8f92417a9941952
Mahmoud Yahia <66545982+mahmoud96yehia@users.noreply.github.com>
2024-01-31 16:04:37 +0200

    Add files via upload

---
+++ JavaScript.js
@@ -0,0 +1,60 @@
+﻿
+function AddName() {
+
+    var name = document.getElementById('name').value;
+    document.getElementById('namehere').innerHTML = name;
+
+
+}
+var material = ['math', 'science', 'Arabic', 'English'];
+var Sdegree = [];
+var Sindex = 0;
+function addDegree() {
+
+    Sdegree[Sindex] = parseInt( document.getElementById("degreee").value);
+    Sindex++;
+    document.getElementById("degreee").value = '';
+
+    var tbl = '<table><tr><th> Degree </th></tr>';
+
+    for (var i = 0; i < Sdegree.length; i++) {
+      
+        tbl += '<tr> <td>' + material[i] + ' = ' + Sdegree[i] + '</td></tr>';
+    }
+    tbl += '</table>';
+    document.getElementById('studentmark').innerHTML = tbl;
+   
+        
+
+}
+function calcDegree() {
+
+    let totaldegree = 0;
+    for (var i = 0; i < Sdegree.length; i++) {
+
+        totaldegree += Sdegree[i];
+
+    }
+
+    let avg = totaldegree / Sdegree.length;
+
+    document.getElementById('total').innerText = avg;
+
+    if (avg < 60) {
+
+        document.getElementById('answer').innerText = "F";
+
+    }
+    else if (avg < 70) {
+        document.getElementById('answer').innerText = "D";
+    }
+    else if (avg < 80) {
+        document.getElementById('answer').innerText = "C";
+    }
+    else if (avg < 90) {
+        document.getElementById('answer').innerText = "B";
+    }
+    else if (avg < 100) {
+        document.getElementById('answer').innerText = "A";
+    }
+}
